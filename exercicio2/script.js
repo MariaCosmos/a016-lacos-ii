d7f6fade8cdd8b3f960732854e143d54e42d0480
MariaCosmos <cosmos.maria2003@gmail.com>
2022-09-13 15:34:15 -0300

    Exercícios concluídos - laços II

---
+++ exercicio2/script.js
@@ -0,0 +1,10 @@
+
+let num = 6
+let baseTabuada = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] 
+
+for(i in baseTabuada){
+  console.log(`${num} x ${baseTabuada[i]} = ${num*baseTabuada[i]}`)
+}
+
+
+
---
+++ exercicio3/script.js
@@ -0,0 +1,7 @@
+const todosPaises = ["Rússia", "Canadá", "China", "EUA", "Brasil"]
+let contador = 0
+
+for(paises of todosPaises){
+  console.log(`${contador} - ${paises}`)
+  contador++
+}
